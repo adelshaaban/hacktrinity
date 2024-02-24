3262e0ea43eef260952aa03c382e91d7fde790c4
gouthamj <gouthamj@qburst.com>
2024-02-24 16:15:53 +0530

    requirements file added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile
 
 app = FastAPI()
 
 @app.get("/api")
 async def get_serever_status():
     return "Server is running successfully"
+
+@app.post("/api")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
