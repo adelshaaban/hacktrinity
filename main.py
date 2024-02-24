ca65efcfafec261ea089900eb050833ad7a3fc4f
gouthamj <gouthamj@qburst.com>
2024-02-24 16:53:19 +0530

    video part added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI,UploadFile
+from fastapi import FastAPI,UploadFile,File
+import uuid
+
+ImagePath = "images/"
 
 app = FastAPI()
 
@@ -7,5 +10,12 @@ async def get_serever_status():
     return "Server is running successfully"
 
 @app.post("/api")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile = File(...)):
+    # file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+
+    #save the file
+    with open(f"{ImagePath}{file.filename}", "wb") as f:
+        f.write(contents)
+
     return {"filename": file.filename}
