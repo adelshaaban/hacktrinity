6ce9399a5ec0f9ae749f93193fcc43e9b16b4e83
gouthamj <gouthamj@qburst.com>
2024-02-24 16:58:55 +0530

    cors added

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI,UploadFile,File
+from fastapi.middleware.cors import CORSMiddleware
 import uuid
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
 
 ImagePath = "images/"
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api")
 async def get_serever_status():
